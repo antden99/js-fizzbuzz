319fc3f4c5dc08a0fd7cd8c399b79a8f2134d605
antden99 <antoniodenigris@icloud.com>
2024-02-21 17:47:59 +0100

    add style

--- bonus/assets/main.js
+++ bonus/assets/main.js
@@ -8,8 +8,32 @@ const ul = document.createElement("ul");
 container.append(ul);
 console.log(ul);
 
-for(let i=1; i<=100 ; i++){
+for (let i = 1; i <= 100; i++) {
     let elementList = document.createElement("li");
     ul.append(elementList);
-    elementList.innerHTML = `${i}`
+
+    let valore=i
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log(i + " è un multiplo di 3 e di 5");
+        valore = "fizzbuzz";
+        classe = "fizzbuzz";
+    }
+    //creo un else if che controlla se il numero è un multiplo di 3
+    else if (i % 3 === 0) {
+        console.log(i + " è un multiplo 3");
+        valore = "fizz";
+        classe = "fizz";
+    } //controllo se il numero è multiplo di 5
+
+    else if (i % 5 === 0) {
+        console.log(i + " è un multiplo 5");
+        valore = "buzz";
+        classe = "buzz";
+    }
+    elementList.innerHTML = `${valore}`
+    elementList.style.color="black";
+    elementList.style.backgroundColor="red";
+    elementList.style.borderRadius="10px";
+    elementList.style.textAlign="center";
+
 }
