066d4b365f9e7d2317b344d631f9f53b0950bd9e
antden99 <antoniodenigris@icloud.com>
2024-02-21 14:11:56 +0100

    add for cicle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 console.log("tutto ok");
+
+// creo una variabile a cui assegno l'outerHTML ul con l'id list e verifico in console
+const listElement=document.getElementById("list");
+console.log(listElement);
+
+//creo un ciclo for che stampi i numeri da 0 a 100 in console e nel document
+for(let i=0;i<=100;i++){
+    console.log(i);
+    listElement.insertAdjacentHTML("beforeend",`<li>${i}</li>`);
+}
